3c88aa38985db44967da645b0b354dba7e6f31f8
sugitlab <sgmt.snj@gmail.com>
2022-08-20 16:56:24 +0900

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:fl_icon_pkg/main.dart';
+import 'package:fluent_emoji_pkg/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
